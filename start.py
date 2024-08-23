111be10a0e959e2fe3b11c9980872d15262c1004
vigneshperiasamy <143394493+vigneshperiasamy@users.noreply.github.com>
2024-08-23 05:55:51 +0000

    issue

--- start.py
+++ start.py
@@ -443,7 +443,7 @@ def initialize_bot(bot, bot_id):
     def check_ping(message):
         start_time = time.time()
         bot.reply_to(message, "Pong!")
-        ping = (time.time() - start_time) * 1000 / 5
+        ping = (time.time() - start_time) * 1000 / 15
         bot.send_message(message.chat.id, f"Bot Ping : {ping:.2f} ms")
     
     @bot.message_handler(commands=['rules'])
